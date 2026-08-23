943ae6bd1acb83645c029d8dce87ababe77c6795
3ddie-papi <epiceddiekim@gmail.com>
2026-08-23 12:53:34 +0300

    Fixing the API issue

--- input.py
+++ input.py
@@ -1,23 +1,29 @@
 import argparse
 from decimal import Decimal
-from forex import api_call
-
+import requests
+import json
 parser = argparse.ArgumentParser()
+def main():
+  parser.add_argument("-a", "--amount", type=float, nargs = 1, required = True, help = "Amount to be converted")
+  parser.add_argument("-f", "--fro", type= str, nargs =1, required = True, help = "Currency the money is being converted from")
+  parser.add_argument("-t", "--to", type =str, nargs = 1, required = True, help = "Currency the money is being converted to")
 
-parser.add_argument("-a", "--amount", type=float, nargs = 1, required = True, help = "Amount to be converted")
-parser.add_argument("-f", "--fro", type= str, nargs =1, required = True, help = "Currency the money is being converted from")
-parser.add_argument("-t", "--to", type =str, nargs = 1, required = True, help = "Currency the money is being converted to")
+  args = parser.parse_args()
 
-args = parser.parse_args()
+  base_currency = "".join(map(str, args.fro))
+  quote_currency = "".join(map(str, args.to))
 
-base_currency = "".join(map(str, args.fro))
-quote_currency = "".join(map(str, args.to))
+  amount_to_be_converted = Decimal("".join(map(str, args.amount)))
+  get_exchange_rate(base_currency, quote_currency, amount_to_be_converted)
 
-amount_to_be_converted = Decimal("".join(map(str, args.amount)))
+def get_exchange_rate(base, quote, amount):
+  response = requests.get(f"https://v6.exchangerate-api.com/v6/c7f741e93737d04413c795d9/pair/{base}/{quote}").json()
 
-exchange_rate = format(Decimal(api_call(base_currency, quote_currency)), ".5g")
+  print(json.dumps(response, indent=4))
+
+if __name__ == "__main__":
+  main()
 
 
 
 
-print(f"Your money from {base_currency} to {quote_currency} is now {Decimal(amount_to_be_converted) * Decimal(exchange_rate):.2f} {quote_currency}")
