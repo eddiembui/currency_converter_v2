009e14bea475f27638388ec830fe9336072fe821
3ddie-papi <epiceddiekim@gmail.com>
2026-08-24 14:25:59 +0300

    Added some error handling

--- currency_converter.py
+++ currency_converter.py
@@ -10,6 +10,11 @@ from dotenv import load_dotenv
 load_dotenv()
 api_key = os.getenv("API_URL")
 parser = argparse.ArgumentParser()
+
+
+class LimitError(Exception):
+  pass
+
 def main():
   parser.add_argument("-a", "--amount", type=float, nargs = 1, required = True, help = "Amount to be converted")
   parser.add_argument("-f", "--fro", type= str, nargs =1, required = True, help = "Currency the money is being converted from")
@@ -36,6 +41,12 @@ def get_exchange_rate(base, quote, amount):
           
   except (FileNotFoundError, json.JSONDecodeError):
     response = requests.get(f"{api_key}{base}/{quote}").json()
+    
+    if response["error-type"] == "unsupported-code":
+      raise ValueError("Wrong currency code entered!")
+    elif response["error-type"] == "quota-reached":
+      raise LimitError("API requests limit reached!")
+    
     json_data = {"base_code": response["base_code"], "target_code": response["target_code"], "conversion_rate": response["conversion_rate"]}
     with open("rates.json", "w") as file:
       json.dump([json_data], file, indent=4)
@@ -47,6 +58,12 @@ def update_cache_file(filename, base, quote):
   with open(filename, "r") as file:
     json_data = json.load(file)
   response = requests.get(f"{api_key}{base}/{quote}").json()
+
+  if response["error-type"] == "unsupported-code":
+    raise ValueError("Wrong currency code entered!")
+  elif response["error-type"] == "quota-reached":
+    raise LimitError("API requests limit reached!")
+  
   info = {"base_code": response["base_code"], "target_code": response["target_code"], "conversion_rate": response["conversion_rate"]}
   json_data.append(info)
   with open(filename, "w") as file:
